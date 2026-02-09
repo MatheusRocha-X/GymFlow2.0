54fa29e6c15b5ddd58a65dd16c82652a67987cdd
MatheusRocha-X <xmatheusx2003@gmail.com>
2026-02-09 18:34:55 -0300

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'icon-192.png', 'icon-512.png'],
+      includeAssets: ['favicon.ico', 'gymflow-logo.png', 'icon-192.png', 'icon-512.png'],
       manifest: {
         name: 'GymFlow - Treinos e Hidratação',
         short_name: 'GymFlow',
